9d4f836aae54e466d3ccfc7a3bbbd5db862b0161
hanumanman <hanumanman37@gmail.com>
2024-01-29 23:34:14 +0700

    remove alpha header, toggle word wrap, switch theme & add mini.surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.motion.mini-surround" },
 }
